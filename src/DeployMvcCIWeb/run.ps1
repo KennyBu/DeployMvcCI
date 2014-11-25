f522f49c460ba7d3e259771cd607450211ba3925
Ken Burkhardt <ken.burkhardt@gmail.com>
2014-11-25 13:50:35 -0500

    Build with Powershell

---
+++ src/DeployMvcCIWeb/run.ps1
@@ -0,0 +1,7 @@
+﻿$proj = “.\DeployMvcCIWeb.csproj”
+
+$azure = “.\Properties\PublishProfiles\prof.pubxml”
+
+$pwd = “XrD7gZhPNBh8TlPZy0ajGAyBszW5oPzb3NujuhGf1QaQQXTYWhNtPhWxCfst”
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $proj /p:DeployOnBuild=true /p:PublishProfile=Production /p:PublishProfile=$azure /p:VisualStudioVersion=12.0 /p:Password=$pwd /p:AllowUntrustedCertificate=true /p:OutputPath=bin\Debug
